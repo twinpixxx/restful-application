255d21074a1f695af9a122ad12ffdd79454f17d5
Anthony Borushko <twinpixxx2005@gmail.com>
2020-03-14 11:33:45 +0300

    fix(TriangleController): remove useless .ui.Model things
    Remove model things. Tried to run fronted (in work).

--- src/main/java/com/example/restservice/TriangleController.java
+++ src/main/java/com/example/restservice/TriangleController.java
@@ -3,7 +3,6 @@ package com.example.restservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 
 @RestController
 public class TriangleController {
@@ -12,10 +11,7 @@ public class TriangleController {
     @GetMapping("/triangle")
     public Triangle greeting(@RequestParam(value = "a") int firstSide
                                 , @RequestParam(value = "b") int secondSide
-                                , @RequestParam(value = "c") int thirdSide, Model model) {
-        model.addAttribute("firstSide", firstSide);
-        model.addAttribute("secondSide", secondSide);
-        model.addAttribute("thirdSide", thirdSide);
+                                , @RequestParam(value = "c") int thirdSide) {
         return new Triangle(firstSide, secondSide, thirdSide);
     }
 }
