c0a76bf31eb06ebd4378f14b5be111240ac19c6a
Anthony Borushko <twinpixxx2005@gmail.com>
2020-03-25 22:47:38 +0300

    feat: add API Exception
    Add Request exception handler and proto of exception

---
+++ src/main/java/com/labs/restservice/exception/ApiException.java
@@ -0,0 +1,34 @@
+package com.labs.restservice.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
+
+public class ApiException {
+
+    private final String massage;
+    //private final Throwable throwable;
+    private final HttpStatus httpstatus;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    public ApiException(String massage, HttpStatus httpstatus, LocalDateTime timestamp) {
+        this.massage = massage;
+        this.httpstatus = httpstatus;
+        this.timestamp = timestamp;
+    }
+
+    public String getMassage() {
+        return massage;
+    }
+
+
+    public HttpStatus getHttpstatus() {
+        return httpstatus;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
+
