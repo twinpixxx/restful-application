ad64d0d36bccac1c3d9d0d26ac002b835393376c
Anthony Borushko <twinpixxx2005@gmail.com>
2020-05-03 18:38:38 +0300

    feat: add cache service
    Add totally new `TriangleCacheService` class that uses
    `HashMap<Triangle, CalculationResults>` as in memory results for params
    that already been used.

---
+++ src/main/java/com/labs/restservice/cache/TriangleCacheService.java
@@ -0,0 +1,29 @@
+package com.labs.restservice.cache;
+
+import com.labs.restservice.calculations.CalculationResults;
+import com.labs.restservice.triangle.Triangle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service("TriangleCacheService")
+public class TriangleCacheService {
+    private static final Logger log = LoggerFactory.getLogger(TriangleCacheService.class);
+    private HashMap<Triangle, CalculationResults> trianlgeCalculationCache = new HashMap<>();
+
+    public TriangleCacheService() {}
+
+    public void add (Triangle _params, CalculationResults _results) {
+        trianlgeCalculationCache.put(_params, _results);
+    }
+
+    public boolean contains (Triangle _params) {
+        return trianlgeCalculationCache.containsKey(_params);
+    }
+
+    public CalculationResults getResults (Triangle _params) {
+        return trianlgeCalculationCache.get(_params);
+    }
+}
