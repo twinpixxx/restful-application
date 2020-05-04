c9e94d483e72b831b467f76a5082fa91b958426a
Anthony Borushko <twinpixxx2005@gmail.com>
2020-05-04 03:58:12 +0300

    feat: add `ServiceAccessManager` that provides concurrency
    Implement `ServiceAccessManager` that add some concurrency to any
    services and counts requests. Uses mutex for concurrent counting of
    requests. It also helps to reduce simultaneous load of any services.

---
+++ src/main/java/com/labs/restservice/concurrent/ServiceAccessManager.java
@@ -0,0 +1,32 @@
+package com.labs.restservice.concurrent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service("ServiceAccessManager")
+public class ServiceAccessManager {
+    private static final Logger logger = LogManager.getLogger();
+
+    private static long requestCount;
+    private ReentrantLock mutex;
+
+    public ServiceAccessManager() {
+        mutex = new ReentrantLock();
+    }
+
+    public void requestCounter() {
+        try {
+            mutex.lock();
+            requestCount += 1;
+            logger.info("Total count of requests = " + requestCount);
+        } finally {
+            mutex.unlock();
+        }
+    }
+
+    public long getRequestCount() {
+        return requestCount;
+    }
+}
