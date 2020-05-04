6ee69563392371a90964eed60220602a2c2d4322
Anthony Borushko <twinpixxx2005@gmail.com>
2020-05-04 03:58:51 +0300

    feat: add `ServiceAccessManager` to `TriangleController`

--- src/main/java/com/labs/restservice/triangle/TriangleController.java
+++ src/main/java/com/labs/restservice/triangle/TriangleController.java
@@ -3,6 +3,7 @@ package com.labs.restservice.triangle;
 import com.labs.restservice.cache.TriangleCacheService;
 import com.labs.restservice.calculations.CalculationResults;
 import com.labs.restservice.calculations.CalculationService;
+import com.labs.restservice.concurrent.ServiceAccessManager;
 import com.labs.restservice.exception.ApiException.ApiRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +22,9 @@ public class TriangleController {
     @Autowired
     private TriangleCacheService cache;
 
+    @Autowired
+    private ServiceAccessManager accessManager;
+
     private static final Logger log = LoggerFactory.getLogger(TriangleController.class);
 
     @GetMapping("/triangle")
@@ -34,6 +38,7 @@ public class TriangleController {
                 (thirdSide <= 0)) {
             throw new ApiRequestException("Triangle side(s) should be positive");
         }
+        accessManager.requestCounter();
         Triangle triangle =  new Triangle(firstSide, secondSide, thirdSide);
         CalculationResults results = new CalculationResults();
         if (cache.contains(triangle)) return cache.getResults(triangle);
